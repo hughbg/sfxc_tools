1e68c786a0a40dccf9d81bfd14017f4e9ee07815
Hugh Garsden <hugh.garsden@manchester.ac.uk>
2025-07-14 13:45:26 +0100

    ctrl inputs scan

--- inspect_sfxgpu_ctrl_inputs.py
+++ inspect_sfxgpu_ctrl_inputs.py
@@ -6,4 +6,41 @@ if len(sys.argv) != 2:
 
 lines = open(sys.argv[1]).readlines()
 
-print(len(lines))
+fields = []
+
+for l in lines:
+    while "ctrl[\"" in l:
+        where = l.find("ctrl[")+6
+        where_end = where
+        while l[where_end] != '"': where_end += 1
+        field = l[where:where_end]
+        if field not in fields: fields.append(field)
+
+        l = l[where_end:]
+
+
+print("Fields expected in control file:\n\n", fields)
+
+import json
+
+with open("/home/v66437hg/merlin_git/sfxc/run/sfxctest.ctrl") as f:
+
+    params = json.load(f)
+print()
+print("Expected fields present in control file:\n")
+for p in params:
+    if p in fields:
+        print(p+":", params[p])
+
+print()
+print("Expected fields missing from control file:\n")
+for f in fields:
+    if f not in params:
+        print(f)
+
+print()
+print("Unexpected fields in control file:\n")
+for p in params:
+    if p not in fields:
+        print(p+":", params[p])
+
